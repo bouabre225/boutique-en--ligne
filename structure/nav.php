bfba17865566b4e09e7b1b9ca6f6983dc72d42c2
Ange KORE <angebouabre463@gmail.com>
2025-04-03 22:25:36 +0100

    ajout d'un menu burger stylé lorqu'on reduit l'ecran

--- structure/nav.php
+++ structure/nav.php
@@ -1,7 +1,22 @@
+<div class="nav-container">
+    <button class="burger-menu" onclick="toggleMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+    <div class="nav-links">
+        <a href="index.php?page=acceuil">Page d'acceuil</a>
+        <a href="index.php?page=detail">Détails du produits</a>
+        <a href="index.php?page=commande">Panier</a>
+        <a href="index.php?page=confirmation">Confirmation de commande</a>
+        <a href="index.php?page=ajout">Ajouter au catalogue</a>
+        <a href="index.php?page=contact">Nous Contacter</a>
+    </div>
+</div>
 
-    <a href="index.php?page=acceuil">Page d'acceuil</a>
-    <a href="index.php?page=detail">Détails du produits</a>
-    <a href="index.php?page=commande">Panier</a>
-    <a href="index.php?page=confirmation">Confirmation de commande</a>
-    <a href="index.php?page=ajout">Ajouter au catalogue</a>
-    <a href="index.php?page=contact">Nous Contacter</a>
+<script>
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    document.querySelector('.burger-menu').classList.toggle('active');
+}
+</script>
