226304c5e3279b4db3c8ed53d0d06c5181ef6011
Ange KORE <angebouabre463@gmail.com>
2025-04-03 22:45:55 +0100

    ajout des attributs pour le css

--- ajout.php
+++ ajout.php
@@ -4,39 +4,39 @@
     <title>Ajouter un produit</title>
 </head>
 <body>
-    <h1>Ajouter un produit</h1>
-    <form action="./traitementAjout.php" method="post" enctype="multipart/form-data">
-        <div>
+    <h1 class="text-center">Ajouter un produit</h1>
+    <form action="./traitementAjout.php" method="post" enctype="multipart/form-data" class="form">
+        <div class="form-group">
             <label for="name" class="form-label">Nom du produit</label>
-            <input type="text" name="name" id="name" required>
+            <input type="text" name="name" id="name" required class="form-control">
         </div>
 
-        <div>
+        <div class="form-group">
             <label for="description" class="form-label">Détails</label>
             <textarea name="description" id="description" required class="form-control"></textarea>
         </div>
 
-        <div>
-            <label for="categorie">Categorie</label>
-            <select name="categorie" id="categorie" required>
+        <div class="form-group">
+            <label for="categorie" class="form-label">Categorie</label>
+            <select name="categorie" id="categorie" required class="form-control">
                 <option value="Fruit">Fruit</option>
                 <option value="Legume">Legume</option>
             </select>
         </div>
 
-        <div>
+        <div class="form-group">
             <label for="image" class="form-label">Image</label>
             <input type="file" name="image" id="image" accept="image/jpeg,image/png,image/gif" required class="form-control">
         </div>
 
-        <div>
-            <label for="prix">Prix</label>
+        <div class="form-group">
+            <label for="prix" class="form-label">Prix</label>
             <input type="number" name="prix" id="prix" step="0.01" required class="form-control">
         </div>
 
-        <div>
-            <label for="remise">Remise</label>
-            <input type="number" name="remise" id="remise" required>
+        <div class="form-group">
+            <label for="remise" class="form-label">Remise</label>
+            <input type="number" name="remise" id="remise" required class="form-control">
         </div>
 
         <button type="submit" class="btn btn-ajouter">Ajouter le produit</button>
